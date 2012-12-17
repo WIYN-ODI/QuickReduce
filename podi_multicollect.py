6c726c53e0689d60eda3cad8187a88d477b8d727
Ralf Kotulla <kotulla@wisc.edu>
2012-12-17 16:27:11 -0600

    Added new tool multicollect as a wrapper around collectcells. This tool can collectcells on a number of input directories, with output files written either in the current directory (default), in a user-specified directory (use -output /dir/name) or in the directory from which the files are read (use -keeppath, but be careful to not mess up the pODI archive).

---
+++ podi_multicollect.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+#
+# (c) Ralf Kotulla for WIYN/pODI
+#
+
+"""
+How-to use:
+
+./podi_collectcells.py 
+
+
+"""
+
+import sys
+import os
+import pyfits
+import numpy
+import scipy
+
+gain_correct_frames = False
+from podi_definitions import *
+from podi_collectcells import *
+
+
+if __name__ == "__main__":
+
+    # Loop over all files and collect all cells. Name output files after the basename of the input file.
+    
+    start = 1
+    output_directory = None
+    keeppath = False
+    if (sys.argv[1] == "-output"):
+        output_directory = sys.argv[2]
+        start = 3
+        if (not os.path.isdir(output_directory)):
+            os.mkdir(output_directory)
+    elif (sys.argv[1] == "-keeppath"):
+        keeppath = True
+        start = 2
+        
+    # Handle all reduction flags from command line
+    bias_dir, dark_dir, flatfield_dir, bpm_dir, start = read_reduction_directories(start=start, warn=False)
+
+    print start
+    for folder in sys.argv[start:]:
+
+        if (folder[-1] == "/"):
+            folder = folder[:-1]
+
+        directory,basename = os.path.split(folder)
+        if (directory == ""):
+            directory = "."
+            
+        if (output_directory != None):
+            outputfile = "%s/%s.fits" % (output_directory, basename)
+        elif (keeppath):
+            outputfile = "%s/%s.fits" % (directory, basename)
+        else:
+            outputfile = "%s.fits" % (basename)
+
+        print folder, directory, basename, "-->",outputfile
+
+
+        # Collect all cells, perform reduction and write result file
+        collectcells(folder, outputfile,
+                     bias_dir, dark_dir, flatfield_dir, bpm_dir)
+    
+    stdout_write("Yippie, completely done!\n")
+    
