127aaf8a29a2e3ac63b29ad3a0a8c70d51032653
Ralf Kotulla <kotulla@wisc.edu>
2012-12-18 06:02:29 -0600

    Moved the shdmem_as_ndarray routine into the podi_definitions file to make it more generally available.

--- podi_imcombine.py
+++ podi_imcombine.py
@@ -30,38 +30,6 @@ except:
 from podi_definitions import *
 
    
-from types import *   
-def shmem_as_ndarray( raw_array ):
-    _ctypes_to_numpy = {
-        ctypes.c_char : numpy.int8,
-        ctypes.c_wchar : numpy.int16,
-        ctypes.c_byte : numpy.int8,
-        ctypes.c_ubyte : numpy.uint8,
-        ctypes.c_short : numpy.int16,
-        ctypes.c_ushort : numpy.uint16,
-        ctypes.c_int : numpy.int32,
-        ctypes.c_uint : numpy.int32,
-        ctypes.c_long : numpy.int32,
-        ctypes.c_ulong : numpy.int32,
-        ctypes.c_float : numpy.float32,
-        ctypes.c_double : numpy.float64
-    }
-    address = raw_array._wrapper.get_address()
-    size = raw_array._wrapper.get_size()
-    dtype = _ctypes_to_numpy[raw_array._type_]
-    class Dummy(object): pass
-    d = Dummy()
-    d.__array_interface__ = {
-         'data' : (address, False),
-         'typestr' : ">f4", #FloatType, #"uint8", #numpy.uint8.str,
-         'descr' : "", #"UINT8", #numpy.uint8.descr,
-         'shape' : (size/4,),
-         'strides' : None,
-         'version' : 3
-    }
-    return numpy.asarray(d)#.view( dtype=numpy.float32 )
-
-
 
 def parallel_compute(queue, shmem_buffer, shmem_results, size_x, size_y, len_filelist):
     #queue, shmem_buffer, shmem_results, size_x, size_y, len_filelist = worker_args
