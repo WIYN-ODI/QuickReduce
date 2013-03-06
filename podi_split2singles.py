53ff39c040577a8bd9042d4cc7de441a485e7ed0
Ralf Kotulla <kotulla@wisc.edu>
2013-03-06 16:35:35 -0600

    Added new tool to split a multi-extension file into its individual components.

---
+++ podi_split2singles.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+#
+# (c) Ralf Kotulla for WIYN/pODI
+#
+
+import sys
+import os
+import pyfits
+import numpy
+
+if __name__ == "__main__":
+
+    filename = sys.argv[1]
+    print filename
+
+    hdulist = pyfits.open(filename)
+
+    for extension in range(1, len(hdulist)):
+
+        extname = hdulist[extension].header['EXTNAME']
+        if (extname[0:3] != "OTA" or extname[-3:] != "SCI"):
+            continue
+
+        outfits = filename[:-4]+extname+".fits"
+
+        primhdu = pyfits.PrimaryHDU(header=hdulist[extension].header,
+                                    data=hdulist[extension].data)
+        out_hdulist = pyfits.HDUList([primhdu])
+        out_hdulist.writeto(outfits, clobber=True)
