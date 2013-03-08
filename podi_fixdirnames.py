363f97a0ca81464ccd8a04ce29347c76146fccd3
Ralf Kotulla <kotulla@wisc.edu>
2013-03-08 11:28:25 -0600

    Added a new tool to rename the directories from the name the archive gives to the obsid-string used at the telescope.

---
+++ podi_fixdirnames.py
@@ -0,0 +1,45 @@
+#!/usr/local/bin/python
+
+#
+# (c) Ralf Kotulla for WIYN/pODI
+#
+
+"""
+How-to use:
+
+./podi_collectcells.py 
+
+
+"""
+
+import sys
+import os
+
+
+
+if __name__ == "__main__":
+
+    for dirname in sys.argv[1:]:
+        if (not os.path.isdir(dirname)):
+            continue
+
+        dirlist = os.listdir(dirname)
+
+        # Check for the .33.fits file
+        for filename in dirlist:
+            if (filename.find("33.fits")):
+                basename = filename[:18]
+                #print dirname,"-->",basename
+
+                if (os.path.isdir(basename)):
+                    print "Directory",basename,"already exists, skipping!"
+                else:
+                    print "Renaming directory %s --> %s" % (dirname, basename)
+                    mv_cmd = "mv %s %s" % (dirname, basename)
+                    os.system(mv_cmd)
+
+                break
+    print "All done!\n"
+
+
+
