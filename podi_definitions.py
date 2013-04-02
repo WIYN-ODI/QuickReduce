95b2dc979cf56064203f0a64401ba7a3d6967388
Ralf Kotulla <kotulla@wisc.edu>
2013-04-02 16:37:22 -0500

    Fixed problem with different versions from different machines.

--- podi_definitions.py
+++ podi_definitions.py
@@ -438,27 +438,3 @@ def log_svn_version(hdr):
 
 
 
-
-
-#################################3
-#
-# Some definitions for colors 
-#
-#################################3
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot
-import matplotlib.colors
-from matplotlib.colors import LinearSegmentedColormap
-
-colorfade_bluewhite = {'red':   ((0.0, 1.0, 1.0),
-                                 (1.0, 142./255., 1.)),
-
-                       'green': ((0.0, 1.0, 1.0),
-                                 (1.0, 163./255., 0.0)),
-
-                       'blue':  ((0.0, 1.0, 1.0),
-                                 (1.0, 218./255., 0.0))
-                       }
-cmap_bluewhite = matplotlib.colors.LinearSegmentedColormap('BlueWhite', colorfade_bluewhite)
-matplotlib.pyplot.register_cmap(cmap=cmap_bluewhite)
