3428eb5f6c902d476265213f590941f9548ce6c4
Ralf Kotulla <kotulla@wisc.edu>
2013-07-26 11:25:00 -0500

    Minor changes to workings: match_catalog now breaks down the catalog into subregions and calls match_catalog_segment to do the actual matching. Previously match_catalogs only worked on sub-catalogs.

--- podi_matchcatalogs.py
+++ podi_matchcatalogs.py
@@ -9,12 +9,39 @@ from podi_definitions import *
 
 arcsec = 1./3600.
 
-def match_catalogs(ref_full, odi_full, ra_ranges, dec_ranges, matching_radius=2):
+def match_catalogs(ref_full, odi_full, matching_radius=2, verbose=False):
 
+    ref_ra_min, ref_ra_max = numpy.min(ref_full[:,0]), numpy.max(ref_full[:,0])
+    ref_dec_min, ref_dec_max = numpy.min(ref_full[:,1]), numpy.max(ref_full[:,1])
+
+    dummy, ra_ranges = numpy.histogram([], bins=5, range=(ref_ra_min, ref_ra_max))
+    dummy, dec_ranges = numpy.histogram([], bins=5, range=(ref_dec_min, ref_dec_max))
+
+    matched_cat = None
+    for cur_ra in range(ra_ranges.shape[0]-1):
+        for cur_dec in range(dec_ranges.shape[0]-1):
+
+            # Select one of the ranges and hand the parameters off to the matching routine
+            matched_cat_segment = match_catalog_segment(ref_full, odi_full, 
+                                                        ra_ranges[cur_ra:cur_ra+2], 
+                                                        dec_ranges[cur_dec:cur_dec+2],
+                                                        matching_radius=matching_radius,
+                                                        verbose=verbose)
+            
+            if (matched_cat_segment == None):
+                continue
+
+            matched_cat = matched_cat_segment if (matched_cat == None) \
+                else numpy.append(matched_cat, matched_cat_segment, axis=0)
+
+    return matched_cat
+
+def match_catalog_segment(ref_full, odi_full, ra_ranges, dec_ranges, 
+                          matching_radius=2, verbose=False):
 
     # First of all extract only the reference stars in the selected box
-    ref_select = (ref_full[:,0] >  ra_ranges[0]) & (ref_full[:,0] <  ra_ranges[1]) \
-               & (ref_full[:,1] > dec_ranges[0]) & (ref_full[:,1] < dec_ranges[1])
+    ref_select = (ref_full[:,0] >  ra_ranges[0]) & (ref_full[:,0] <=  ra_ranges[1]) \
+               & (ref_full[:,1] > dec_ranges[0]) & (ref_full[:,1] <= dec_ranges[1])
 
     ref = ref_full[ref_select]
 
@@ -30,7 +57,10 @@ def match_catalogs(ref_full, odi_full, ra_ranges, dec_ranges, matching_radius=2)
                & (odi_full[:,1] > dec_ranges[0]-d_dec) & (odi_full[:,1] < dec_ranges[1]+d_dec)
     odi = odi_full[odi_select]
 
-    print "Searching for reference stars: RA: %3.1f - %3.1f,  DEC: %+4.1f - %+4.1f" % (ra_ranges[0]-d_ra, ra_ranges[1]+d_ra,dec_ranges[0]-d_dec,dec_ranges[1]+d_dec),
+    if (verbose):
+        print "Searching for reference stars: RA: %3.1f - %3.1f,  DEC: %+4.1f - %+4.1f" % (
+            ra_ranges[0]-d_ra, ra_ranges[1]+d_ra,dec_ranges[0]-d_dec,dec_ranges[1]+d_dec),
+
     # Create the output array, and set all ODI position to the "not found" standard value
     output_array = numpy.zeros(shape=(ref.shape[0], ref.shape[1]+odi.shape[1]))
     output_array[:, 2:4] = -99999.9
@@ -61,7 +91,8 @@ def match_catalogs(ref_full, odi_full, ra_ranges, dec_ranges, matching_radius=2)
                 output_array[star, odi_data_start:] = odi[si[0], 2:]
                 matches_found += 1
 
-    print "  -->    %5d matches (%5d vs %5d)" % (matches_found, ref.shape[0], odi.shape[0])
+    if (verbose):
+        print "  -->    %5d matches (%5d vs %5d)" % (matches_found, ref.shape[0], odi.shape[0])
     
     return output_array
 
