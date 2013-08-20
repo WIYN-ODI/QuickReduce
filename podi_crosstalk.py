b4b14b30639eaca1d848777e7af534751324a752
Ralf Kotulla <kotulla@wisc.edu>
2013-08-20 13:27:35 -0500

    Added copyright information to all files. No code changes.

--- podi_crosstalk.py
+++ podi_crosstalk.py
@@ -1,3 +1,24 @@
+#
+# Copyright 2012-2013 Ralf Kotulla
+#                     kotulla@uwm.edu
+#
+# This file is part of the ODI QuickReduce pipeline package.
+#
+# If you find this program or parts thereof please make sure to
+# cite it appropriately (please contact the author for the most
+# up-to-date reference to use). Also if you find any problems 
+# or have suggestiosn on how to improve the code or its 
+# functionality please let me know. Comments and questions are 
+# always welcome. 
+#
+# The code is made publicly available. Feel free to share the link
+# with whoever might be interested. However, I do ask you to not 
+# publish additional copies on your own website or other sources. 
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#
 
 x0 =  5.6E-5 #1.2e-4
 
