1d3f29235d4d647b91a0f2e786e7cf551de2f555
Ralf Kotulla <kotulla@wisc.edu>
2013-08-23 13:19:17 -0500

    Added a few test versions of methods to determine the ideal fringe scaling.

--- podi_fringing.py
+++ podi_fringing.py
@@ -28,7 +28,8 @@ import numpy
 numpy.seterr(divide='ignore', invalid='ignore')
 import scipy
 import scipy.stats
-
+import scipy.optimize
+import bottleneck
 
 import Queue
 import threading
@@ -37,6 +38,7 @@ import ctypes
 
 from podi_definitions import *
 import podi_imcombine
+import podi_fitskybackground
 
 avg_sky_countrates = {
     "odi_i": 3.8,
@@ -162,6 +164,19 @@ def make_fringing_template(input_filelist, outputfile, return_hdu=False, skymode
 
     return
 
+
+def match_subtract_fringing(datahdu, fringe):
+
+    if (not type(fringe) is pyfits.HDUList):
+        # The fringe variable is a filename
+        fringe_filename = fringe
+        fringe = pyfits.open(fringe_filename)
+        
+
+    return
+
+
+
 if __name__ == "__main__":
 
     if (cmdline_arg_isset("-singles")):
@@ -222,14 +237,15 @@ if __name__ == "__main__":
 
         print filename,"-->",outputfile
         hdulist = pyfits.open(filename)
-        for i in range(len(hdulist)):
-            if (is_image_extension(hdulist[i].header)):
+        for i in range(3,len(hdulist)):
+            if (True): #is_image_extension(hdulist[i].header)):
                 extname = hdulist[i].header['EXTNAME']
                 stdout_write("\rWorking on %s ..." % (extname))
                 regions = numpy.array(podi_fitskybackground.sample_background(hdulist[i].data, None, None, min_found=count, boxwidth=boxsize))
                 
                 median = numpy.median(regions[:,4])
                 std = numpy.std(regions[:,4])
+
 
                 histogram, binedges = numpy.histogram(regions[:,4], bins=500, range=(median-20*std,median+20*std))
                 nice_histogram = numpy.empty(shape=(histogram.shape[0], 3))
@@ -239,14 +255,18 @@ if __name__ == "__main__":
                 
                 dumpfile = "%s.%s" % (outputfile, extname)
                 df = open(dumpfile, "w")
-                numpy.savetxt(df, regions)
-                print >>df, "\n\n\n\n\n\n\n"
-                numpy.savetxt(df, nice_histogram)
-                print >>df, "\n\n\n\n\n\n\n"
+                #numpy.savetxt(df, regions)
+                #print >>df, "\n\n\n\n\n\n\n"
+                #numpy.savetxt(df, nice_histogram)
+                #print >>df, "\n\n\n\n\n\n\n"
 
                 validpixels = hdulist[i].data
-                fullframe, edges = numpy.histogram(validpixels, bins=500, range=(median-20*std,median+20*std))
-                nice_histogram[:,2] = fullframe[:]
+                histogram, binedges = numpy.histogram(validpixels, bins=1000, range=(median-10*std,median+10*std))
+                nice_histogram = numpy.empty(shape=(histogram.shape[0], 3))
+                nice_histogram[:,0] = binedges[:-1]
+                nice_histogram[:,1] = binedges[1:]
+                nice_histogram[:,2] = histogram[:]
+
                 numpy.savetxt(df, nice_histogram)
 
                 df.close()
@@ -283,5 +303,202 @@ if __name__ == "__main__":
         
         stdout_write(" done!\n")
 
+    if (cmdline_arg_isset("-optimize")):
+        dataframe = get_clean_cmdline()[1]
+        fringemap = get_clean_cmdline()[2]
+        
+        datahdu = pyfits.open(dataframe)
+        fringehdu = pyfits.open(fringemap)
+
+        for ext in range(1,14): #len(datahdu)):
+
+            # Load data for each extension/OTA
+            extname = datahdu[ext].header['EXTNAME']
+            print "\n\n\n",extname,"\n"
+            data = datahdu[extname].data
+            fringe = fringehdu[extname].data
+
+            # rebin data to cut down on processing time
+            binning = 16
+            data_binned = rebin_image(data, binning)
+            fringe_binned = rebin_image(fringe, binning)
             
+            output_hdulist = [pyfits.PrimaryHDU(data = fringe_binned)]
+            output_hdulist[0].header.update("EXTNAME", "MAP")
+
+            valid_both = numpy.isfinite(data_binned) & numpy.isfinite(fringe_binned)
+            data_binned = data_binned[valid_both]
+            fringe_binned = fringe_binned[valid_both]
+
+            skylevel = datahdu[ext].header['SKY_MEDI']
+
+            data_binned -= skylevel
+
+            percent_limit = 15
+
+            # Now select top 10 and bottom 10% of all intensities in the fringe map
+            top10 = scipy.stats.scoreatpercentile(fringe_binned.ravel(), 100-percent_limit)
+            bottom10 = scipy.stats.scoreatpercentile(fringe_binned.ravel(), percent_limit)
+            print top10, bottom10
+            #sys.exit(0)
+
+            select_top10 = fringe_binned >= top10
+            select_bottom10 = fringe_binned <= bottom10
+            #fringe_top10 = numpy.median(fringe_binned[select_top10])
+            #fringe_bottom10 = numpy.median(fringe_binned[select_bottom10])
+
+#            x = fringe.copy()
+#            output_hdulist.append(pyfits.ImageHDU(data=x))
+#            x = fringe.copy()
+#            x[x < top10] = numpy.NaN
+#            output_hdulist.append(pyfits.ImageHDU(data=x))
+#            x = fringe.copy()
+#            x[x > bottom10] = numpy.NaN
+#            output_hdulist.append(pyfits.ImageHDU(data=x))
+
+#            x = data.copy()
+#            output_hdulist.append(pyfits.ImageHDU(data=x))
+#            x = data.copy()
+#            x[fringe < top10] = numpy.NaN
+#            output_hdulist.append(pyfits.ImageHDU(data=x))
+#            x = data.copy()
+#            x[fringe > bottom10] = numpy.NaN
+#            output_hdulist.append(pyfits.ImageHDU(data=x))
+
+            
+            def clip3sigma(data):
+                med, sigma = 0, 1e9
+                valid = numpy.isfinite(data)
+                for rep in range(3):
+                    med = numpy.median(data[valid])
+                    sigma = 0.5 * (scipy.stats.scoreatpercentile(data[valid], 84) - 
+                                   scipy.stats.scoreatpercentile(data[valid], 16))
+                    valid = (data < med + 3*sigma) & (data > med - 3*sigma)
+                return numpy.median(data[valid])
+
+            fringe_top10 = clip3sigma(fringe_binned[select_top10])
+            fringe_bottom10 = clip3sigma(fringe_binned[select_bottom10])
+
+            # Now get intensities in the original frame for the same pixels
+            #sky_top10 = numpy.median(data_binned[select_top10])
+            #sky_bottom10 = numpy.median(data_binned[select_bottom10])
+            sky_top10 = clip3sigma(data_binned[select_top10])
+            sky_bottom10 = clip3sigma(data_binned[select_bottom10])
+
+            #sys.exit(0)
+
+            print top10, bottom10
+            print fringe_top10, fringe_bottom10, fringe_top10-fringe_bottom10
+            print sky_top10, sky_bottom10, sky_top10-sky_bottom10
+            fringe_scaling = (sky_top10-sky_bottom10)/(fringe_top10-fringe_bottom10)
+            print "scaling = ", fringe_scaling
+
+            def save_histogram(target, data, nbins=100):
+                valid = numpy.isfinite(data)
+                med = numpy.median(data[valid])
+                ls, us = scipy.stats.scoreatpercentile(data[valid], 16), scipy.stats.scoreatpercentile(data[valid], 84)
+                sigma = 0.5 * (us - ls)
+                min = med - 3*sigma
+                max = med + 3*sigma
+                histogram, binedges = numpy.histogram(data[valid], bins=nbins, range=(min,max))
+                nice_histogram = numpy.empty(shape=(histogram.shape[0], 3))
+                nice_histogram[:,0] = binedges[:-1]
+                nice_histogram[:,1] = binedges[1:]
+                nice_histogram[:,2] = histogram[:]
+                numpy.savetxt(target, nice_histogram)
+
+            dump = open("xxx.dump"+extname, "w")
+            save_histogram(dump, fringe_binned[select_top10])
+            print >>dump, "\n\n\n\n\n"
+            save_histogram(dump, fringe_binned[select_bottom10])
+            print >>dump, "\n\n\n\n\n"
+            save_histogram(dump, data_binned[select_top10])
+            print >>dump, "\n\n\n\n\n"
+            save_histogram(dump, data_binned[select_bottom10])
+            dump.close()
+
+#            output_hdulist.append(pyfits.ImageHDU(data=data))
+#            #output_hdulist.append(pyfits.ImageHDU(data=(data - fringe_scaling * fringe)))
+#            output_hdulist.append(pyfits.ImageHDU(data=(data - 0.5*fringe_scaling * fringe)))
+
+#            outhdulist = pyfits.HDUList(output_hdulist)
+#            outhdulist.writeto("output.fits", clobber=True)
+
+            datahdu[ext].data -= skylevel #fringe_scaling * fringe
+
+        datahdu.writeto("corrected.fits", clobber=True)
+
+        sys.exit(0)
+
+
+
+    if (cmdline_arg_isset("-rmfringe")):
+        dataframe = get_clean_cmdline()[1]
+        fringemap = get_clean_cmdline()[2]
+        
+        datahdu = pyfits.open(dataframe)
+        fringehdu = pyfits.open(fringemap)
+
+        all_results = None
+        for ext in range(1,14): #len(datahdu)):
+
+            # Load data for each extension/OTA
+            extname = datahdu[ext].header['EXTNAME']
+            print "\n\n\n",extname,"\n"
+            data = datahdu[extname].data
+            fringe = fringehdu[extname].data
+
+            # rebin data to cut down on processing time
+            binning = 8
+            data_binned = rebin_image(data, binning)
+            fringe_binned = rebin_image(fringe, binning)
+
+            # compute mean fringe amplitude
+            mean_fringe = bottleneck.nanmean(fringe_binned)
+            fringe_binned -= mean_fringe
+            print "mean fringe =",mean_fringe
+
+            skylevel = datahdu[ext].header['SKY_MEDI']
+            skysub = data_binned - skylevel
+
+            def min_stat(scale, data, fringe):
+                return bottleneck.nanmean( numpy.fabs((skysub - scale*fringe)*fringe) )
+                #return bottleneck.nanmean( ((skysub - scale*fringe)*fringe)**2 )
+
+            initial_guess = 100
+            res = scipy.optimize.fmin(min_stat, initial_guess, 
+                                      args=(skysub, fringe_binned),
+                                      full_output=True)
+            print res
+            res_fits = [[res[0][0], res[1], res[2], res[3], res[4]]]
+            all_results = numpy.array(res_fits) if all_results == None else numpy.append(all_results, res_fits, axis=0)
+
+#            xopt, fopt, iter, funcalls, warnflag, allvecs = res
+#            print xopt
+ 
+#            for s in range(0,2000,25):
+#                print extname, s, min_stat(s, skysub, fringe_binned)
+
+            #print "success=",res.success
+            #print "res.X=",res.x
+            #print "msg=",res.message
+            #print "\n\n"
+
+            
+
+        print all_results
+
+        # Now that we got all best-fit factors, keep only the 
+        # best 5 fits
+        quality_sorted = numpy.sort(all_results[:,1])
+        use_for_median = all_results[:,1] < quality_sorted[5]
+        
+        median_scaling = numpy.median(all_results[:,0][use_for_median])
+        std_scaling = numpy.std(all_results[:,0][use_for_median])
+
+        print median_scaling, std_scaling
+
+
+        #datahdu.writeto("corrected.fits", clobber=True)
+
         sys.exit(0)
