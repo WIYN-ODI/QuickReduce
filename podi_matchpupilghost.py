6717c85b0f48f85712926419c546a46d7a2f4272
Ralf Kotulla <kotulla@wisc.edu>
2013-09-05 14:59:25 -0500

    Added first implementation of a pupilghost removal from science frames. Matching is done by matching the pupilghost scaling to the light excess measured in the science frame.

--- podi_matchpupilghost.py
+++ podi_matchpupilghost.py
@@ -37,6 +37,7 @@ import scipy
 
 gain_correct_frames = False
 from podi_definitions import *
+import podi_fitskybackground
 
 
 scaling_factors = {
@@ -190,12 +191,149 @@ def create_azimuthal_template(filename, outputfilename):
     return
 
 
+def get_pupilghost_scaling(science_frame, pupilghost_frame, 
+                n_samples=750, boxwidth=20, 
+                verbose=True):
+
+    sci_hdulist = pyfits.open(science_frame) if (type(science_frame) == str) else science_frame
+    pg_hdulist = pyfits.open(pupilghost_frame) if (type(pupilghost_frame) == str) else pupilghost_frame
+
+    merged_all = numpy.zeros(shape=(0,6))
+
+    for ext in range(1,5):
+        extname = sci_hdulist[ext].header['EXTNAME']
+        filter = sci_hdulist[0].header['FILTER']
+        
+        center = pupilghost_centers[filter][extname]
+
+        data = sci_hdulist[ext].data
+        pg = pg_hdulist[extname].data
+        
+        #n_samples = 1750
+        samples = numpy.array(podi_fitskybackground.sample_background(data, None, None, 
+                                                          min_found=n_samples, fit_regions=[], 
+                                                          boxwidth=boxwidth))
+
+        box_centers = samples[:,2:4]
+        print box_centers.shape
+
+        # Now convert the x/y from the science frame 
+        # into x/y in the pupil ghost frame
+        dxy = box_centers - center
+        pg_xy = dxy + [ 0.5*pg.shape[0], 0.5*pg.shape[1] ]
+
+        #print dxy[0:10,:]
+        #print pg_xy[0:10,:]
+
+        #print pg_samples.shape
+        print pg_xy.shape
+        pg_samples =  numpy.array(podi_fitskybackground.sample_background(pg, None, None, 
+                                                                          fit_regions=[],
+                                                             box_center=pg_xy))
+#        pg_samples = podi_fitskybackground.sample_background(pg, None, None, 
+#                                                             box_center=box_centers)
+
+        print pg_samples.shape
+
+        #continue
+
+        ri = 600
+        ro = 3000
+        radius = numpy.sqrt(numpy.sum( dxy**2, axis=1))
+        #print radius[0:10]
+
+        print extname
+        print center
+
+        numpy.savetxt("xxx", samples)
+        numpy.savetxt("yyy", pg_samples)
+
+        # unaffected data points inside or outside the pupil ghost
+        unaffected = (radius < ri) | (radius > ro)
+        skylevel = numpy.median(samples[:,4][unaffected])
+        print "skylevel=",skylevel
+        print numpy.median(samples[:,4])
+
+        # derive maximum intensity of pupil ghost 
+        # better: read from file
+        pg_max = numpy.max(pg_samples[:,4])
+
+        # Now merge the two arrays:
+        merged = numpy.empty(shape=(pg_xy.shape[0],6))
+        merged[:,0:2] = samples[:,2:4]
+        merged[:,2:4] = pg_samples[:,2:4]
+        merged[:,4] = numpy.array(samples)[:,4] - skylevel
+        merged[:,5] = numpy.array(pg_samples)[:,4]
+        numpy.savetxt("zzz", merged)
+
+        merged_all = numpy.append(merged_all, merged, axis=0)
+
+        ratio = merged[:,4] / merged[:,5]
+        
+        strong_pg_signal = merged[:,5] > 0.4*pg_max
+
+        valid_ratios = ratio[strong_pg_signal]
+
+        median_ratio = numpy.median(valid_ratios)
+        print "median_ratio =",median_ratio
+        print "error = ",numpy.std(valid_ratios)
+        numpy.savetxt("merged%d" % (ext), merged[strong_pg_signal])
+
+
+    print "\n\n============================\n\n"
+    ratio = merged_all[:,4] / merged_all[:,5]
+        
+    pg_max = numpy.max(merged_all[:,5])
+    strong_pg_signal = merged_all[:,5] > 0.4*pg_max
+
+    valid_ratios = ratio[strong_pg_signal]
+
+    import podi_collectcells
+    good_ratios = podi_collectcells.three_sigma_clip(ratio[strong_pg_signal])
+
+    median_ratio = numpy.median(valid_ratios)
+    print "median_ratio =",median_ratio
+    print "error = ",numpy.std(valid_ratios)
+    print "clipped median",numpy.median(good_ratios)
+    print "clipped std",numpy.std(good_ratios)
+    numpy.savetxt("merged_all", merged_all[strong_pg_signal])
+    numpy.savetxt("merged_clipped", good_ratios)
+
+    clipped_median = numpy.median(good_ratios)
+    clipped_std = numpy.std(good_ratios)
+    return clipped_median, clipped_std
+
 if __name__ == "__main__":
 
     if (cmdline_arg_isset("-makeradial")):
         inputframe = get_clean_cmdline()[1]
         outputframe = get_clean_cmdline()[2]
         create_azimuthal_template(inputframe, outputframe)
+        sys.exit(0)
+
+    if (cmdline_arg_isset("-getscaling")):
+        science_frame = get_clean_cmdline()[1]
+        pupilghost_frame = get_clean_cmdline()[2]
+        #get_scaling(science_frame, pupilghost_frame)
+
+        sci_hdu = pyfits.open(science_frame)
+        pg_hdu = pyfits.open(pupilghost_frame)
+        get_pupilghost_scaling(sci_hdu, pg_hdu)
+        sys.exit(0)
+
+    if (cmdline_arg_isset("-cleanpupilghost")):
+        science_frame = get_clean_cmdline()[1]
+        pupilghost_frame = get_clean_cmdline()[2]
+        output_frame = get_clean_cmdline()[3]
+        #get_scaling(science_frame, pupilghost_frame)
+
+        sci_hdu = pyfits.open(science_frame)
+        pg_hdu = pyfits.open(pupilghost_frame)
+        scaling, scale_std = get_pupilghost_scaling(sci_hdu, pg_hdu)
+        subtract_pupilghost(sci_hdu, pg_hdu, scaling=scaling, rotate=False)
+        
+        sci_hdu.writeto(output_frame, clobber=True)
+
         sys.exit(0)
 
     # Read filenames from command line
