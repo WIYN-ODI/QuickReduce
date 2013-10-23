2643cf9fee483b583427048cd033196abe3d22e6
Ralf Kotulla <kotulla@wisc.edu>
2013-10-23 10:24:03 -0500

    in cellbycell_map: Added check if extension is an image extension.

--- podi_nonlinearity.py
+++ podi_nonlinearity.py
@@ -541,6 +541,10 @@ def plot_cellbycell_map(fitfile, outputfile, minmax, labels=True, fontsize=2):
     fig, ax = matplotlib.pyplot.subplots()
 
     for ext in range(1, len(hdulist)):
+        if (not ((type(hdulist[ext]) == pyfits.hdu.image.ImageHDU) or
+                  type(hdulist[ext]) == pyfits.hdu.compressed.CompImageHDU) ):
+            continue
+
         for cellx in range(8):
             for celly in range(8):
 
