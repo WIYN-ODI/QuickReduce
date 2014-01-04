823d4c042ab0048394dc8f646a77e63c38047af7
Ralf Kotulla <kotulla@wisc.edu>
2014-01-04 01:02:50 -0600

    Added a new function template to handle the conversion of the remote directory names into local filenames.

--- podi_SAMPsetup.py
+++ podi_SAMPsetup.py
@@ -14,9 +14,12 @@ ssh_host = "localhost"
 ssh_executable = "/work/podi_devel/podi_collectcells.py"
 
 def translate_filename_local2remote(filename):
+    """
 
-    # This is a template to re0write the filenames from local paths to the
-    # path used on the remote system
+    This is a template to re0write the filenames from local paths to the
+    path used on the remote system
+
+    """
 
     if (filename.startswith("/Volumes")):
         # This is on a Mac
@@ -25,3 +28,28 @@ def translate_filename_local2remote(filename):
         new_filename = filename
 
     return new_filename
+
+
+
+
+
+def translate_filename_remote2local(input_filename, remote_filename):
+    """
+
+    This is a template to re-write the filenames from the remote machine to 
+    paths valid on the local machine.
+
+    """
+
+    if (remote_filename.startswith("/mnt")):
+        # This is on a Linux Machine, convert to Mac
+        local_filename = filename.replace("/mnt/", "/Volumes/")
+    else:
+        local_filename = filename
+
+    if (local_filename.find("%") >= 0):
+        # This filename contains some special formatting instructions
+        # Go ahead and translate them
+        local_filename = podi_collectcells.format_filename(input_filename, local_filename)
+
+    return local_filename
