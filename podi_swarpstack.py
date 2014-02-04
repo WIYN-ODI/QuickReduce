d9c51a1d5a65a74ffc4da236777ffb4f188c315a
Ralf Kotulla <kotulla@wisc.edu>
2014-02-04 16:17:03 -0600

    Added a little more functionality: using the -reference=xxx.fits option, the output file will have the same settings as the reference frame. This is useful e.g. for matching Halpha exposures to broad-band data.

--- podi_swarpstack.py
+++ podi_swarpstack.py
@@ -13,9 +13,15 @@ swarp_exec = "swarp"
 tmp_dir = "/scratch/"
 single_dir = "."
 
+import podi_logging
+import logging
 
-if __name__ == "__main__":
-    
+
+
+def swarpstack():
+
+    logger = logging.getLogger("SwarpStack - Prepare")
+
     # Figure out the config path
     abspath = os.path.abspath(sys.argv[0])
     dirname, filename = os.path.split(abspath)
@@ -32,7 +38,7 @@ if __name__ == "__main__":
 
     add_only = cmdline_arg_isset("-add") and os.path.isfile(stacked_output)
     if (add_only):
-        print "Activating ADD mode"
+        logger.info("Activating ADD mode")
 
     if (stacked_output.endswith(".fits")):
         stacked_output = stacked_output[:-5]
@@ -40,7 +46,32 @@ if __name__ == "__main__":
     header_only_file = "%s/preswarp.fits" % (tmp_dir)
 
     reference_file = cmdline_arg_set_or_default("-reference", None)
-    if (not add_only):
+    # Make sure the reference file is a valid file
+    if (not reference_file == None and os.path.isfile(reference_file)):
+        logger.info("Using %s as reference file" % (reference_file))
+    else:
+        reference_file = None
+
+    if (add_only or not reference_file == None):
+        #
+        # This is the simpler add-only mode
+        #
+
+        if (not reference_file == None):
+            output_info = pyfits.open(reference_file)
+        else:
+            # Open the existing output header and get data from there
+            output_info = pyfits.open(stacked_output+".fits")
+
+        logger.info("Stack information...")
+        logger.info("   Output-dimensions: %(NAXIS1)5d x %(NAXIS2)5d" % (output_info[0].header))
+
+        out_crval1 = output_info[0].header['CRVAL1']
+        out_crval2 = output_info[0].header['CRVAL2']
+        out_naxis1 = output_info[0].header['NAXIS1']
+        out_naxis2 = output_info[0].header['NAXIS2']
+
+    else:
         #
         # This is the regular start-from-scratch mode
         #
@@ -61,7 +92,7 @@ if __name__ == "__main__":
 
         swarp_opts += " -SUBTRACT_BACK %s " % ("Y" if subtract_back else "N")
 
-        print swarp_opts
+        logger.debug("SWARP options for pre-stack:\n"+" ".join(swarp_opts.split()))
 
         # 
         # First create only the output header so we can pass some information 
@@ -72,19 +103,27 @@ if __name__ == "__main__":
             'opts': swarp_opts,
             'files': " ".join(inputfiles),
         }
-        print swarp_cmd
+        logger.debug("swarp_cmd=\n"+swarp_cmd)
+
 
         try:
-            retcode = subprocess.Popen(swarp_cmd.split(), 
-                                       stdout=subprocess.PIPE, 
-                                       stderr=subprocess.PIPE) #, shell=True)
+            ret = subprocess.Popen(swarp_cmd.split(), 
+                                   stdout=subprocess.PIPE, 
+                                   stderr=subprocess.PIPE) #, shell=True)
             # if retcode < 0:
             #     print >>sys.stderr, "Child was terminated by signal", -retcode
             # else:
             #     print >>sys.stderr, "Child returned", retcode
             #print retcode.stdout.readlines()
             #print retcode.stderr.readlines()
+            (swarp_stdout, swarp_stderr) = ret.communicate()
+            logger.debug("swarp stdout:\n"+swarp_stdout)
+            if (len(swarp_stderr) > 0 and ret.returncode != 0):
+                logger.warning("swarp stderr:\n"+swarp_stderr)
+            else:
+                logger.debug("swarp stderr:\n"+swarp_stderr)
         except OSError as e:
+            log_exception()
             print >>sys.stderr, "Execution failed:", e
 
         #
@@ -99,21 +138,6 @@ if __name__ == "__main__":
         out_crval2 = output_info[0].header['CRVAL2']
         out_naxis1 = output_info[0].header['NAXIS1']
         out_naxis2 = output_info[0].header['NAXIS2']
-    else:
-        #
-        # This is the simpler add-only mode
-        #
-        
-        # Open the existing output header and get data from there
-        output_info = pyfits.open(stacked_output+".fits")
-        print "Stack information..."
-        print "   Output-dimensions: %(NAXIS1)5d x %(NAXIS2)5d" % (output_info[0].header)
-
-        out_crval1 = output_info[0].header['CRVAL1']
-        out_crval2 = output_info[0].header['CRVAL2']
-        out_naxis1 = output_info[0].header['NAXIS1']
-        out_naxis2 = output_info[0].header['NAXIS2']
-
         
 
     
@@ -121,6 +145,7 @@ if __name__ == "__main__":
     # Prepare the individual frames, rectified and re-projected 
     # to the final grid
     #
+    logger = logging.getLogger("SwarpStack - Singles")
     single_prepared_files = []
     for singlefile in inputfiles:
         hdulist = pyfits.open(singlefile)
@@ -141,52 +166,60 @@ if __name__ == "__main__":
            }
 
         swarp_opts = """\
--c $(swarp_default)s \
--IMAGEOUT_NAME %(singledir)s/%(obsid)s.fits \
--WEIGHTOUT_NAME %(singledir)s/%(obsid)s.weight.fits \
--PIXEL_SCALE %(pixelscale)f \
--PIXELSCALE_TYPE %(pixelscale_type)s \
--COMBINE Y \
--COMBINE_TYPE AVERAGE \
--CENTER_TYPE MANUAL \
--CENTER %(center_ra)f,%(center_dec)f \
--IMAGE_SIZE %(imgsizex)d,%(imgsizey)d \
--RESAMPLE_DIR %(resample_dir)s \
--SUBTRACT_BACK N \
-%(inputfile)s \
-""" % dic
+                 -c $(swarp_default)s \
+                 -IMAGEOUT_NAME %(singledir)s/%(obsid)s.fits \
+                 -WEIGHTOUT_NAME %(singledir)s/%(obsid)s.weight.fits \
+                 -PIXEL_SCALE %(pixelscale)f \
+                 -PIXELSCALE_TYPE %(pixelscale_type)s \
+                 -COMBINE Y \
+                 -COMBINE_TYPE AVERAGE \
+                 -CENTER_TYPE MANUAL \
+                 -CENTER %(center_ra)f,%(center_dec)f \
+                 -IMAGE_SIZE %(imgsizex)d,%(imgsizey)d \
+                 -RESAMPLE_DIR %(resample_dir)s \
+                 -SUBTRACT_BACK N \
+                 %(inputfile)s \
+                 """ % dic
 
         single_file = "%(singledir)s/%(obsid)s.fits" % dic
 
         # print swarp_opts
         swarp_cmd = "%s %s" % (swarp_exec, swarp_opts)
-        print "Preparing file %s, please wait ..." % (singlefile)
-        print swarp_cmd
 
-        if ((add_only or reuse_singles) and os.path.isfile(single_file)):
-            print "This single-swarped file (%s) exist, skipping it" % (single_file)
+        if (add_only and os.path.isfile(single_file)):
+            logger.info("This single-swarped file (%s) exist, skipping it" % (single_file))
+        elif (reuse_singles and os.path.isfile(single_file)):
+            logger.info("This single-swarped file (%s) exist, re-using it" % (single_file))
+            single_prepared_files.append(single_file)
         else:
-#        if (not reuse_singles or not os.path.isfile(single_file)):
+            logger.info("Preparing file %s, please wait ..." % (singlefile))
+            logger.debug(" ".join(swarp_cmd.split()))
             try:
                 ret = subprocess.Popen(swarp_cmd.split(), 
                                        stdout=subprocess.PIPE, 
                                        stderr=subprocess.PIPE)
                 (swarp_stdout, swarp_stderr) = ret.communicate()
-                print swarp_stdout
-                print swarp_stderr
+                logger.debug("swarp stdout:\n"+swarp_stdout)
+                if (len(swarp_stderr) > 0 and ret.returncode != 0):
+                    logger.warning("swarp stderr:\n"+swarp_stderr)
+                else:
+                    logger.debug("swarp stderr:\n"+swarp_stderr)
+                
                 #print "\n".join(swarp_stderr)
                 single_prepared_files.append(single_file)
             except OSError as e:
+                log_exception()
                 print >>sys.stderr, "Execution failed:", e
-#        else:
 
 
+    #
     # If in "add" mode, rename the previous output file and add it to the list of input files
+    #
     if (add_only):
 
         if (len(single_prepared_files) < 1):
-            print "No new files were added, so there's nothing to do."
-            sys.exit(0)
+            logger.info("No new files were added, so there's nothing to do.")
+            return
 
         prev = 1
         while (True):
@@ -199,11 +232,20 @@ if __name__ == "__main__":
         # Rename the current output file and its weights
         old_stacked = "%s.prev%02d.fits" % (stacked_output, prev)
         old_weight = "%s.prev%02d.weight.fits" % (stacked_output, prev)
+
         os.rename(stacked_output+".fits", old_stacked)
+        logger.debug("renamed old stack %s -> %s" % (stacked_output+".fits", old_stacked))
+
         os.rename(stacked_output+".weight.fits", old_weight)
+        logger.debug("renamed old stack weight %s -> %s" % (stacked_output+".weight.fits", old_weight))
 
         # Also add the new re-named old stacked file to list of input files
         single_prepared_files.append(old_stacked)
+        logger.debug("Adding now old stack file to input list")
+    #
+    # Done re-naming the old file
+    #
+
 
     #
     # Now all single files are prepared, go ahead and produce the actual stack
@@ -215,37 +257,60 @@ if __name__ == "__main__":
     dic['bgsub'] = "Y" if subtract_back else "N"
 
     swarp_opts = """\
--c %(swarp_default)s \
--IMAGEOUT_NAME %(imageout)s \
--WEIGHTOUT_NAME %(weightout)s \
--COMBINE_TYPE %(combine_type)s \
--PIXEL_SCALE %(pixelscale)f \
--PIXELSCALE_TYPE %(pixelscale_type)s \
--COMBINE Y \
--COMBINE_TYPE %(combine_type)s \
--CENTER_TYPE MANUAL \
--CENTER %(center_ra)f,%(center_dec)f \
--IMAGE_SIZE %(imgsizex)d,%(imgsizey)d \
--RESAMPLE_DIR %(singledir)s \
--SUBTRACT_BACK %(bgsub)s \
--WEIGHT_TYPE MAP_WEIGHT \
-%(prepared_files)s \
-""" % dic
+                 -c %(swarp_default)s \
+                 -IMAGEOUT_NAME %(imageout)s \
+                 -WEIGHTOUT_NAME %(weightout)s \
+                 -COMBINE_TYPE %(combine_type)s \
+                 -PIXEL_SCALE %(pixelscale)f \
+                 -PIXELSCALE_TYPE %(pixelscale_type)s \
+                 -COMBINE Y \
+                 -COMBINE_TYPE %(combine_type)s \
+                 -CENTER_TYPE MANUAL \
+                 -CENTER %(center_ra)f,%(center_dec)f \
+                 -IMAGE_SIZE %(imgsizex)d,%(imgsizey)d \
+                 -RESAMPLE_DIR %(singledir)s \
+                 -SUBTRACT_BACK %(bgsub)s \
+                 -WEIGHT_TYPE MAP_WEIGHT \
+                 %(prepared_files)s \
+                 """ % dic
 
-    print "\n\n\nStarting final stacking...\n\n\n"
+    logger = logging.getLogger("SwarpStack - FinalStack")
+    logger.info("Starting final stacking...")
     # print swarp_opts
 
     swarp_cmd = "%s %s" % (swarp_exec, swarp_opts)
-    print swarp_cmd
+    logger.debug(" ".join(swarp_cmd.split()))
     try:
         ret = subprocess.Popen(swarp_cmd.split(), 
                                    stdout=subprocess.PIPE, 
                                    stderr=subprocess.PIPE)
         (swarp_stdout, swarp_stderr) = ret.communicate()
-        print swarp_stdout
-        print swarp_stderr
+
+        logger.debug("swarp stdout:\n"+swarp_stdout)
+        if (len(swarp_stderr) > 0 and ret.returncode != 0):
+            logger.warning("swarp stderr:\n"+swarp_stderr)
+        else:
+            logger.debug("swarp stderr:\n"+swarp_stderr)
         #print "\n".join(swarp_stderr)
+        logger.info("done, swarp returned (ret-code: %d)!" % ret.returncode)
     except OSError as e:
+        log_exception()
         print >>sys.stderr, "Execution failed:", e
 
- 
+    return
+
+if __name__ == "__main__":
+
+    # Setup everything we need for logging
+    options = set_default_options()
+    setup_logging(options)
+
+    try:
+        swarpstack()
+    except KeyboardInterrupt, SystemExit:
+        pass
+    except:
+        log_exception()
+        pass
+    finally:
+        shutdown_logging(options)
