81d24747c3aee5db6e16a41cef3619bef3e85d1e
Ralf Kotulla <kotulla@wisc.edu>
2014-02-20 16:17:13 -0600

    Added the cython-version of LACosmic to the cosmicray module.

--- podi_cosmicrays.py
+++ podi_cosmicrays.py
@@ -15,10 +15,11 @@ import itertools
 import podi_sitesetup as sitesetup
 import time
 import multiprocessing
+import podi_cython
 
 
 
-def remove_cosmics(hdu, n_iterations=4):
+def remove_cosmics(hdu, n_iterations=4, method="cy"):
 
     gain = hdu.header['GAIN']
     readnoise = 8 #hdu.header['RON']
@@ -31,17 +32,35 @@ def remove_cosmics(hdu, n_iterations=4):
         # print "working on cell",cx, cy
 
         x1, x2,y1, y2 = cell2ota__get_target_region(cx, cy, binning=1)
-        cell_data = hdu.data[x1:x2, y1:y2]
+        cell_data = hdu.data[y1:y2, x1:x2]
 
-        c = cosmics.cosmicsimage(cell_data, 
-                                 gain=gain, readnoise=readnoise, 
-                                 sigclip = 5.0, sigfrac = 0.3, objlim = 5.0,
-                                 verbose=False)
-        c.run(maxiter=n_iterations)
+        if (method == "py"):
+            c = cosmics.cosmicsimage(cell_data, 
+                                     gain=gain, readnoise=readnoise, 
+                                     sigclip = 5.0, sigfrac = 0.3, objlim = 5.0,
+                                     verbose=False)
+            c.run(maxiter=n_iterations)
 
-        # # Re-insert the data into the full frame
-        corrected[x1:x2, y1:y2] = c.cleanarray
-        mask[x1:x2, y1:y2][c.mask] = 1
+            # # Re-insert the data into the full frame
+            corrected[y1:y2, x1:x2] = c.cleanarray
+            mask[y1:y2, x1:x2][c.mask] = 1
+        elif (method == "cy"):
+
+            # Run cosmic ray removal
+            crj = podi_cython.lacosmics(cell_data.astype(numpy.float64), 
+                                        gain=gain, readnoise=readnoise, 
+                                        niter=int(n_iterations),
+                                        sigclip = 5.0, sigfrac=0.3, objlim=5.0,
+                                        verbose=False)
+            cell_cleaned, cell_mask, cell_saturated = crj
+
+            # Re-insert data into full OTA
+            corrected[y1:y2, x1:x2] = cell_cleaned
+            mask[y1:y2, x1:x2] = cell_mask
+            
+        else:
+            corrected[y1:y2, x1:x2] = cell_data
+            print "Unknown method"
 
     return corrected, mask
 
@@ -64,18 +83,22 @@ def removecosmics_mp(input_queue, return_queue):
             input_queue.task_done()
             break
 
-        hdu, niter, id = task
+        hdu, niter, id, method = task
+        # print hdu
+        # print niter
+        # print id
+        # print method
 
         if ('OTA' in hdu.header):
-            print "Removing cosmics from OTA",hdu.header['OTA']
+            print "Removing cosmics from OTA",hdu.header['OTA'],"using method",method
         else:
             print "removing cosmics"
 
-        fixed, mask = remove_cosmics(hdu)
+        fixed, mask = remove_cosmics(hdu, niter, method)
 
         ota = hdu.header['OTA']
-        pyfits.HDUList([pyfits.PrimaryHDU(data=fixed, header=hdulist[i].header)]).writeto("cleaned_OTA%d.fits" % (ota), clobber=True)
-        pyfits.HDUList([pyfits.PrimaryHDU(data=mask, header=hdulist[i].header)]).writeto("mask_OTA%d.fits" % (ota), clobber=True)
+        pyfits.HDUList([pyfits.PrimaryHDU(data=fixed, header=hdulist[i].header)]).writeto("cleaned_OTA%d.%s.fits" % (ota, method), clobber=True)
+        pyfits.HDUList([pyfits.PrimaryHDU(data=mask, header=hdulist[i].header)]).writeto("mask_OTA%d.%s.fits" % (ota, method), clobber=True)
 
         # Send return data
         ret = (fixed, mask, id)
@@ -96,6 +119,9 @@ if __name__ == "__main__":
         #import yappi
         #yappi.start()
 
+        method = cmdline_arg_set_or_default("-method", "cy")
+        niter = int(cmdline_arg_set_or_default("-niter", 4))
+
         job_queue = multiprocessing.JoinableQueue()
         return_queue = multiprocessing.JoinableQueue()
 
@@ -104,7 +130,7 @@ if __name__ == "__main__":
             if (not is_image_extension(hdulist[i])):
                 continue
 
-            task = (hdulist[i], 4, i)
+            task = (hdulist[i], niter, i, method)
             job_queue.put(task)
             n_jobs += 1
 
