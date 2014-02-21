8a54cd6daa652a57bbc98fe0c7ebae5364a6fa8b
Ralf Kotulla <kotulla@wisc.edu>
2014-02-21 12:05:49 -0600

    Finished a stand-alone module for podi_cosmicrays.

--- podi_cosmicrays.py
+++ podi_cosmicrays.py
@@ -16,41 +16,54 @@ import podi_sitesetup as sitesetup
 import time
 import multiprocessing
 import podi_cython
+import podi_collectcells
+import podi_logging
+import logging
 
 
+def remove_cosmics(data, n_iterations=4, method="cy", 
+                   gain=1.5, readnoise=9,
+                   sigclip = 5.0, sigfrac = 0.3, objlim = 5.0,
+                   verbose=False,
+                   saturation_limit=65000,
+                   binning=1,
+):
 
-def remove_cosmics(hdu, n_iterations=4, method="cy"):
 
-    gain = hdu.header['GAIN']
-    readnoise = 8 #hdu.header['RON']
+    corrected = data.copy()
+    mask = numpy.zeros_like(data)
 
-    corrected = hdu.data.copy()
-    mask = numpy.zeros_like(hdu.data)
+    logger = logging.getLogger("CosmicRayX(%s)" % (method))
+    logger.debug("Parameters: gain=%.3f readnoise=%.3f sigclip=%.2f, sigfrac=%.2f objlim=%.2f sat-limit=%d" % (
+        gain, readnoise, sigclip, sigfrac, objlim, saturation_limit))
 
     # Break up each entire OTA into its cells to see if this is faster
     for cx, cy in itertools.product(range(8),repeat=2):
-        # print "working on cell",cx, cy
+        if (verbose): stdout_write("\rworking on cell %d,%d" % (cx, cy))
 
-        x1, x2,y1, y2 = cell2ota__get_target_region(cx, cy, binning=1)
-        cell_data = hdu.data[y1:y2, x1:x2]
+        x1, x2,y1, y2 = cell2ota__get_target_region(cx, cy, binning=binning)
+        cell_data = data[y1:y2, x1:x2]
 
         if (method == "py"):
+            saturation_limit = saturation_limit if saturation_limit > 0 else 1e10
             c = cosmics.cosmicsimage(cell_data, 
                                      gain=gain, readnoise=readnoise, 
-                                     sigclip = 5.0, sigfrac = 0.3, objlim = 5.0,
+                                     sigclip=sigclip, sigfrac=sigfrac, objlim=objlim,
                                      verbose=False)
             c.run(maxiter=n_iterations)
 
             # # Re-insert the data into the full frame
             corrected[y1:y2, x1:x2] = c.cleanarray
             mask[y1:y2, x1:x2][c.mask] = 1
-        elif (method == "cy"):
+            logger.debug("Found %d cosmics in cell %d,%d" % (numpy.sum(c.mask), cx, cy))
 
+        elif (method == "cy"):
             # Run cosmic ray removal
             crj = podi_cython.lacosmics(cell_data.astype(numpy.float64), 
                                         gain=gain, readnoise=readnoise, 
                                         niter=int(n_iterations),
-                                        sigclip = 5.0, sigfrac=0.3, objlim=5.0,
+                                        sigclip=sigclip, sigfrac=sigfrac, objlim=objlim,
+                                        saturation_limit=saturation_limit,
                                         verbose=False)
             cell_cleaned, cell_mask, cell_saturated = crj
 
@@ -58,21 +71,12 @@ def remove_cosmics(hdu, n_iterations=4, method="cy"):
             corrected[y1:y2, x1:x2] = cell_cleaned
             mask[y1:y2, x1:x2] = cell_mask
             
+            logger.debug("Found %d cosmics in cell %d,%d" % (numpy.sum(cell_mask>0), cx, cy))
         else:
             corrected[y1:y2, x1:x2] = cell_data
-            print "Unknown method"
+            logger.warning("Unknown Crj-X method: %s" % (method))
 
     return corrected, mask
-
-    # # Prepare all structures
-    # c = ext_lacosmics.cosmicsimage(hdu.data, 
-    #                                gain=gain, readnoise=readnoise, 
-    #                                sigclip = 5.0, sigfrac = 0.3, objlim = 5.0)
-
-    # # Run the cosmic removal
-    # c.run(maxiter=4)
-
-    # return c.cleanarray, c.mask
 
 
 def removecosmics_mp(input_queue, return_queue):
@@ -83,22 +87,17 @@ def removecosmics_mp(input_queue, return_queue):
             input_queue.task_done()
             break
 
-        hdu, niter, id, method = task
-        # print hdu
-        # print niter
-        # print id
-        # print method
+        data, niter, id, method, verbose, setup = task
+        gain, readnoise, sigclip, sigfrac, objlim, saturation_limit, binning = setup
 
-        if ('OTA' in hdu.header):
-            print "Removing cosmics from OTA",hdu.header['OTA'],"using method",method
-        else:
-            print "removing cosmics"
+        print "Starting cosmic removal on one extension"
 
-        fixed, mask = remove_cosmics(hdu, niter, method)
-
-        ota = hdu.header['OTA']
-        pyfits.HDUList([pyfits.PrimaryHDU(data=fixed, header=hdulist[i].header)]).writeto("cleaned_OTA%d.%s.fits" % (ota, method), clobber=True)
-        pyfits.HDUList([pyfits.PrimaryHDU(data=mask, header=hdulist[i].header)]).writeto("mask_OTA%d.%s.fits" % (ota, method), clobber=True)
+        fixed, mask = remove_cosmics(data, niter, method, 
+                                     gain=gain, readnoise=readnoise,
+                                     sigclip=sigclip, sigfrac=sigfrac, objlim=objlim,
+                                     saturation_limit=saturation_limit,
+                                     binning=binning,
+                                     verbose=verbose)
 
         # Send return data
         ret = (fixed, mask, id)
@@ -112,15 +111,29 @@ def removecosmics_mp(input_queue, return_queue):
 
 if __name__ == "__main__":
 
-    if (cmdline_arg_isset("-standalone")):
+    if (len(sys.argv) <= 1):
+        print __doc__
+        print cosmics.__doc__
+
+    else:
+
+        options = podi_collectcells.read_options_from_commandline()
+        podi_logging.setup_logging(options)
+
         inputfile = get_clean_cmdline()[1]
         hdulist = pyfits.open(inputfile)
-
+        
         #import yappi
         #yappi.start()
 
         method = cmdline_arg_set_or_default("-method", "cy")
         niter = int(cmdline_arg_set_or_default("-niter", 4))
+        verbose = cmdline_arg_isset("-verbose")
+
+        sigclip = float(cmdline_arg_set_or_default("-sigclip", 5.0))
+        sigfrac = float(cmdline_arg_set_or_default("-sigfrac", 0.3))
+        objlim = float(cmdline_arg_set_or_default("-objlim", 5.0))
+        saturation_limit = float(cmdline_arg_set_or_default("-saturate", 60000))
 
         job_queue = multiprocessing.JoinableQueue()
         return_queue = multiprocessing.JoinableQueue()
@@ -130,7 +143,12 @@ if __name__ == "__main__":
             if (not is_image_extension(hdulist[i])):
                 continue
 
-            task = (hdulist[i], niter, i, method)
+            gain = hdulist[i].header['GAIN'] if 'GAIN' in hdulist[i].header else 1.5
+            readnoise = hdulist[i].header['RDNOISEE'] if 'RDNOISEE' in hdulist[i].header else 8.5
+            binning = hdulist[0].header['BINNING'] if 'BINNING' in hdulist[0].header else 1
+
+            setup = gain, readnoise, sigclip, sigfrac, objlim, saturation_limit, binning
+            task = (hdulist[i].data, niter, i, method, verbose, setup)
             job_queue.put(task)
             n_jobs += 1
 
@@ -164,6 +182,5 @@ if __name__ == "__main__":
         #yappi.get_thread_stats().print_all()
         #yappi.get_func_stats().debug_print() #print_all()
 
-    else:
-        print __doc__
-        print cosmics.__doc__
+        podi_logging.shutdown_logging(options)
+
