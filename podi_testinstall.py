75ec9c342ec85259c24ef992bca5bff7c727ffda
Ralf Kotulla <kotulla@wisc.edu>
2014-02-21 14:54:01 -0600

    Added a new auto-config tool to the podi_testinstall to create the podi_sitesetup.py file. This should make adding options to the siteseup file much easier.

--- podi_testinstall.py
+++ podi_testinstall.py
@@ -52,6 +52,147 @@ def check_package(name):
 
     return False
 
+def ask_for_option(var_name, question, backup, needs_quotes, config_array):
+
+    suggestion = backup
+    if (hasattr(podi_sitesetup, var_name)):
+        suggestion = eval('podi_sitesetup.%s' % (var_name))
+        changed = False
+    else:
+        changed = True
+
+    # Now ask user
+    print "----"
+    print question
+    print "Default:", suggestion
+    try:
+        answer = raw_input("Answer:  ")
+    except KeyboardInterrupt:
+        print "\nTerminating\n"
+        pass
+        sys.exit(0)
+
+    if (len(answer.strip()) <= 0):
+        answer = suggestion
+    
+    # New configuration string
+    if (needs_quotes):
+        config_string = "%s = \"%s\"" % (var_name, answer)
+    else:
+        config_string = "%s = %s" % (var_name, answer)
+
+    config_array.append(config_string)
+
+    return (changed or (not answer == suggestion))
+
+def update_sitesetup():
+
+
+    import sys, os, podi_sitesetup
+    os.system("cp podi_sitesetup.py podi_sitesetup.py.backup")
+    
+    # Now go over all options, ask user for input
+    # Use the current configuration as default value
+    
+    config_array = ["# Your setup parameters are next:"]
+
+    import multiprocessing
+    changes = False
+    changes = changes | ask_for_option('max_cpu_count', 
+                   "Maximum number of CPU cores available for use in reduction", 
+                   multiprocessing.cpu_count(), False, config_array)
+    changes = changes | ask_for_option('wcs_ref_dir', 
+                   "Directory holding the local 2MASS catalog in FITS format", 
+                   "/some/dir", True, config_array)
+    changes = changes | ask_for_option('wcs_ref_type', 
+                   "Type of astrometric reference catalog (leave unchanged)", 
+                   "2mass_nir", True, config_array)
+
+    changes = changes | ask_for_option('sdss_ref_type', 
+                   "Source of SDSS catalog (choose from local, web, stripe82)", 
+                   "local", True, config_array)
+    changes = changes | ask_for_option('sdss_ref_dir', 
+                   "If local SDSS catalog, specify source directory", 
+                   "/some/dir", True, config_array)
+
+    changes = changes | ask_for_option('scratch_dir', 
+                   "Scratch-directory for temporary files (the faster the better)", 
+                   "/tmp", True, config_array)
+
+    changes = changes | ask_for_option('sextractor', 
+                   "Path to SourceExtractor executable (Hint: run 'which sex' in another terminal)", 
+                   "/usr/local/bin/sex", True, config_array)
+    changes = changes | ask_for_option('sex_redirect', 
+                   "Command to re-direct stdout and stderr to /dev/null", 
+                   " >& /dev/null", True, config_array)
+    changes = changes | ask_for_option('sex_delete_tmps', 
+                   "Delete SourceExtractor temporary files after use (choose from True (recommended) or False)", 
+                   "True", False, config_array)
+
+    changes = changes | ask_for_option('diagplot__zeropoint_ZPrange', 
+                   "Vertical range around median in photometric calibation plots (format: [0.3,0.3]", 
+                   "[0.5,0.5]", False, config_array)
+    changes = changes | ask_for_option('diagplot__zeropoint_magrange', 
+                   "SDSS Magnitude range in photometric calibation plots (format: [11, 22]", 
+                   "[11, 21]", False, config_array)
+    changes = changes | ask_for_option('diagplot__zeropointmap_range', 
+                   "Spread around median ZP in photometric zeropoint map (format: [0.3,0.3]", 
+                   "[0.2,0.2]", False, config_array)
+
+    changes = changes | ask_for_option('debug_log_filename', 
+                   "Filename for debug logs (e.g. /tmp/debug.log) ", 
+                   "debug.log", True, config_array)
+    changes = changes | ask_for_option('debug_log_append', 
+                   "Keep adding to debug-log (choose True) or create a new file for each run (False)", 
+                   "True", False, config_array)
+
+    changes = changes | ask_for_option('crj_sigclip', 
+                   "Cosmic Ray rejection: Sigma-clipping threshold", 
+                   5.0, False, config_array)
+    changes = changes | ask_for_option('crj_sigfrac', 
+                   "Cosmic Ray rejection: Fraction of sigma-clipping for nearby neighbors", 
+                   0.3, False, config_array)
+    changes = changes | ask_for_option('crj_objlim', 
+                   "Cosmic Ray rejection: Minimum CR significance above sources", 
+                   5.0, False, config_array)
+    changes = changes | ask_for_option('crj_saturation', 
+                   "Cosmic Ray rejection: Saturation limit (negative disables this feature)", 
+                   55000, False, config_array)
+    changes = changes | ask_for_option('crj_niter', 
+                   "Cosmic Ray rejection: Number of CR rejection iterations (typically in the range 1-4)", 
+                   4, False, config_array)
+    
+    config_array.append("")
+
+    # print config_array
+
+    blank_setup = open("podi_sitesetup.py.blank", "r")
+    lines = blank_setup.readlines()
+    # Now find where to insert variables
+    insert_at = -1
+    for i in range(len(lines)):
+        # print "____%s___" % (lines[i].strip())
+        if (lines[i].startswith("###AUTO-CONFIG-INSERT-HERE")):
+            print "Found insert point"
+            insert_at = i
+            break
+
+    if (changes):
+        print "\n\nThere were some changes to the configuration!"
+        answer = raw_input("Are you sure you want to update the configuration (y/n)? ")
+        if (answer == "y" or answer == "Y"):
+            new_config = open("podi_sitesetup.py", "w")
+            new_config.write("".join(lines[:insert_at]))
+            new_config.write(os.linesep.join(config_array))
+            new_config.write("".join(lines[insert_at+1:]))
+            new_config.close()
+            print "Changes saved!"
+        else:
+            print "Keeping configuration unchanged"
+    else:
+        print "\nNo changes found, keeping current configuration"
+
+
 
 if __name__ == "__main__":
     print
@@ -115,3 +256,15 @@ if __name__ == "__main__":
  python setup.py build_ext --inplace
 
 """
+
+    print "\n"*4,"     Starting auto-configuration!","\n"*4
+    print "Creating backup of current podi_sitesetup.py"
+    import sys, os, podi_sitesetup
+#    os.system("cp podi_sitesetup.py podi_sitesetup.py.backup")
+#    if (not hasattr(podi_sitesetup, 'xxx')):
+#        print "Can't find "
+
+    update_sitesetup()
+
+
+
