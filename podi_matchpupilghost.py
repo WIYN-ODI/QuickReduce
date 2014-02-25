207d4d42c9a1fd53b807c7a433857911cb8c81a3
Ralf Kotulla <kotulla@wisc.edu>
2014-02-25 16:26:33 -0600

    Added two new functions that a) compute the pupilghost template for a given OTA, accounting for rotation. b) compute the scaling factor between sky level and pupilghost template    amplitude.

--- podi_matchpupilghost.py
+++ podi_matchpupilghost.py
@@ -97,6 +97,400 @@ scaling_factors = {
     "odi_i": 0.3, #15,
 }
 
+def compute_pupilghost_template_ota(input_hdu, pupil_hdu, 
+                                    rotate=True, verbose=True, non_negative=True,
+                                    source_center_coords='data'
+                                ):
+
+    """
+
+    Contains all functionality to match and rotate the pupilghost template to 
+    match the current OTA.
+
+    Parameters
+    ----------
+    input_hdu : pyfits.HDU
+        Has to be a valid ImageHDU. This HDU is the source of the image data 
+        that contains the pupilghost, as well as the EXTNAME keyword to select
+        the correct template.
+
+    rotator_angle : float
+        Rotator angle from the primary HDU
+
+    filtername : string
+        This parameter is only important if source_center_coords is not set to
+        'header' or 'data'. In that case, it defines the filter for the set of 
+        set of pre-canned pupilghost center coordinates
+
+    pupil_hdu : pyfits.HDUList
+        HDUlist containing the pupilghost template
+
+    scaling : float
+        scaling factor for the pupilghost amplutide
+
+    rotate : Bool
+        Apply rotation (see rotator_angle) to the pupilghost template before
+        removing it from the input data
+
+    verbose : Bool
+        Output some progress updates during runtime
+
+    non_negative : Bool
+        if set, all negative values in the pupilghost templates (that are 
+        very likely unphysical) are clipped to 0.
+
+    source_center_coords : ('data', 'header', 'xxx').
+        if set to 'data', the center of the pupilghost is determined auto-
+        matically during execution. If set to 'header', it is assumed that
+        the input-hdu already contains header keywords with information about
+        the location of the pupilghost. This is likely not the case for flat-
+        field data, but important for science frames. All other values use
+        the pre-canned coordinates given in podi_definitions.
+
+    Returns
+    -------
+    No return value per se, changes are reflected in input_hdu
+
+    see also
+    --------
+    `dev_pgcenter.find_pupilghost_center`
+
+    """
+
+    #
+    # Check if this extension is included in the pupilghost template
+    #
+    extname = input_hdu.header['EXTNAME']
+    right_ext = -1
+    for this_ext in range(1, len(pupil_hdu)):
+        if (pupil_hdu[this_ext].header['EXTNAME'] == extname):
+            right_ext = this_ext
+            break
+
+    if (right_ext < 0):
+        # This extension is not listed in the pupil-ghost template
+        # We can't do anythin in this case, so let's simply return
+        input_hdu.header['PGAFCTD'] = (False, "is the OTA affected by the pupilghost")
+        return None
+
+    # Get some information about the current input frame
+    rotator_angle = input_hdu.header['ROTSTART'] if (rotate) else 0.
+    filtername = input_hdu.header['FILTER']
+
+    #
+    # Set the coordinates of the center of the pupilghost 
+    #
+    # Read the center coordinates from the pupil ghost template file
+    source_center_coords = 'data'
+    if (source_center_coords == 'data'):
+        fx, fy, fr, vx, vy, vr = dev_pgcenter.find_pupilghost_center(input_hdu, verbose=False)
+        center_x, center_y = vx, vy
+    #
+    # Use center coordinates from header
+    elif (source_center_coords == 'header'):
+        # if ("PGCNTR_X" in input_hdu.header): 
+        #     center_x = input_hdu.header['PGCNTR_X']
+        # if ("PGCNTR_Y" in input_hdu.header): 
+        #     center_y = input_hdu.header['PGCNTR_Y']
+
+        if ('PGEFCTVX' in input_hdu.header):
+            center_x = input_hdu.header['PGEFCTVX']
+        if ('PGEFCTVX' in input_hdu.header):
+            center_x = input_hdu.header['PGEFCTVX']
+    #
+    # by default, resort to the old-fashined canned values
+    else:
+        if (filtername in pupilghost_centers):
+            if (extname in pupilghost_centers[filtername]):
+                print pupilghost_centers[filtername][extname]
+                center_x, center_y = pupilghost_centers[filtername][extname]
+            else:
+                if (extname in pupilghost_centers):
+                    center_x, center_y = pupilghost_centers[extname]
+                else:
+                    # Don't know what center coordinate to use, abort
+                    return None
+        elif (extname in pupilghost_centers):
+            center_x, center_y = pupilghost_centers[extname]
+        else:
+            # Don't know what center coordinate to use, abort
+            return None
+
+    #
+    # Now we know we have to do something
+    #
+    if (verbose): 
+        print "Found matching pupil ghost template in extension",right_ext
+    print "Using center coordinates", center_x, center_y," for data frame"
+
+    if (rotate):
+        # print "___%s___" % rotator_angle
+        if (rotator_angle == "unknown"):
+            rotator_angle = 0.0
+
+        # Rotate to the right angle
+        # Make sure to rotate opposite to the rotator angle since we rotate 
+        # the correction and not the actual data
+        template = pupil_hdu[right_ext].data
+        # Replace all NaNs with zeros
+        template[numpy.isnan(template)] = 0.
+
+        if (math.fabs(rotator_angle) < 0.5):
+            if (verbose): print "Rotator angle is small (%.2f), skipping rotation" % (rotator_angle)
+            rotated = template
+        else:
+            if (verbose): print "rotating template by",rotator_angle,"degrees"
+            rotated = rotate_around_center(template, rotator_angle, mask_nans=False)
+    else:
+        if (verbose): print "No rotation requested, skipping rotation"
+        rotated = pupil_hdu[right_ext].data
+     
+   
+    template_centerx, template_centery = rotated.shape[0]/2, rotated.shape[1]/2
+    if (source_center_coords == 'data'):
+        #
+        # Ok, now we have the pupil ghost rotated to the right angle
+        #
+        data_shape = input_hdu.data.shape
+
+        # Now extract the right quadrant from the pupilghost template
+        quadrant = {'OTA33.SCI': [0,4500,0,4500],
+                    'OTA34.SCI': [0,4500,4500,9000],
+                    'OTA44.SCI': [4500,9000,4500,9000],
+                    'OTA43.SCI': [4500,9000,0,4500],
+                }
+        quadrant = {'OTA33.SCI': [400,4500,400,4500],
+                    'OTA34.SCI': [400,4500,4500,8600],
+                    'OTA44.SCI': [4500,8600,4500,8600],
+                    'OTA43.SCI': [4500,8600,400,4500],
+                }
+        xys = quadrant[extname]
+        template_quadrant = rotated[xys[2]:xys[3], xys[0]:xys[1]]
+        # Now we have the right quadrant, search for center position in template
+        imghdu = pyfits.ImageHDU(data=template_quadrant)
+        imghdu.header['EXTNAME'] = extname
+        pyfits.HDUList([pyfits.PrimaryHDU(), imghdu]).writeto("template_%s.fits" % extname, clobber=True)
+        tx, ty, _, _, _, _ = dev_pgcenter.find_pupilghost_center(imghdu, verbose=False,
+                                                                 fixed_radius=vr)
+        print "Found pg center at",tx, ty
+        
+        template_centerx, template_centery = tx+xys[0], ty+xys[2]
+        print "Using",template_centerx, template_centery, "as center coordinates for template"
+        # center_x, center_y = vx, vy
+        
+        # Swap x/y since python does it too
+        print "rot.shape=",rotated.shape
+
+ 
+    bx = template_centerx - center_x
+    by = template_centery - center_y
+    tx, ty = bx + data_shape[0], by + data_shape[1]
+
+    correction = rotated[by:ty, bx:tx]
+    correction[numpy.isnan(correction)] = 0.
+
+    if (non_negative):
+        correction[correction < 0] = 0
+
+    # Store the position of the pupil ghost center
+    input_hdu.header['PGAFCTD'] = (True, "is the OTA affected by the pupilghost")
+    input_hdu.header['PGCENTER'] = "%d %d" % (center_x, center_y)
+    input_hdu.header['PGCNTR_X'] = (center_x, 'pupil ghost center x')
+    input_hdu.header['PGCNTR_Y'] = (center_y, 'pupil ghost center y')
+    input_hdu.header['PGTMPL_X'] = (template_centerx, 'pupil ghost template center x')
+    input_hdu.header['PGTMPL_Y'] = (template_centery, 'pupil ghost template center y')
+    input_hdu.header['PGREG_X1'] = (bx, "matched region of pupilghost, left")
+    input_hdu.header['PGREG_X2'] = (tx, "matched region of pupilghost, right")
+    input_hdu.header['PGREG_Y1'] = (by, "matched region of pupilghost, bottom")
+    input_hdu.header['PGREG_Y2'] = (ty, "matched region of pupilghost, top")
+    input_hdu.header['PGEFCTVX'] = (center_x - template_centerx + rotated.shape[1]/2, "effective pupilghost center X")
+    input_hdu.header['PGEFCTVY'] = (center_y - template_centery + rotated.shape[0]/2, "effective pupilghost center Y")
+    input_hdu.header['PGROTANG'] = (rotator_angle, "pupil ghost rotator angle")
+
+    return correction
+
+
+def get_pupilghost_scaling_ota(science_hdu, pupilghost_frame, 
+                               n_samples=750, boxwidth=20, 
+                               verbose=True,
+                               pg_matched=False):
+    """Find the optimum scaling for the pupil ghost. This is done by sampling the
+    image frame and the pupil ghost template at a range of identical
+    positions. The median ratio between these measurements then yields the
+    scaling ratio.
+
+    Parameters
+    ----------
+    
+    science_hdu : ImageHDU
+
+        If science frame is a string, it is interpreted as the filename of the
+        science frame for which we are to obtain the scaling. Alternatively you
+        can also pass the HDUList of the science frame.
+
+    pupilghost_template : string of HDUList
+
+        same as above, just for the pupil-ghost template
+
+    n_samples : int
+
+        Number of intensity samples to take *from each OTA* when deriving the
+        optimum scaling factor.
+
+    boxwidth : int
+ 
+        Size of the sample box, in pixels
+
+    Returns
+    -------
+
+    median_scaling_factor
+
+    standard deviation of scaling factor
+
+    """
+
+    logger = logging.getLogger("GetPupilghostScaling")
+
+    merged_all = numpy.zeros(shape=(0,6))
+
+    any_affected = False
+
+
+    
+    extname = science_hdu.header['EXTNAME']
+    filter = science_hdu.header['FILTER']
+
+    if (type(pupilghost_frame) == str):
+        pg_hdulist = pyfits.open(pupilghost_frame)
+        pg = pg_hdulist[extname].data
+    elif (type(pupilghost_frame) == pyfits.HDUList):
+        pg = pupilghost_frame[extname].data
+    elif (type(pupilghost_frame) == numpy.ndarray):
+        pg = pupilghost_frame
+    else:
+        logger.debug("Not sure what the format of the pupilghost is!")
+        return None
+
+    logger.debug("Checking extension %s (filter: %s) for pupilghost effects" % (extname, filter))
+
+    if (not 'PGAFCTD' in science_hdu.header or not science_hdu.header['PGAFCTD']):
+        # This frame does not contain the keyword labeling it as affected by
+        # the pupilghost. In that case we don't need to do anything
+        logger.debug("This extension (%s) does not have any pupilghost problem" % (extname))
+        return None
+
+    logger.debug("Checking pupilghost center position")
+
+    # Try to find the center position of the pupilghost from the science
+    # frame. The necessary headers should have been inserted during 
+    # flat-field correction
+    if ('PGEFCTVX' in science_hdu.header and 
+        'PGEFCTVY' in science_hdu.header):
+
+        center = (science_hdu.header['PGEFCTVX'], 
+                  science_hdu.header['PGEFCTVY'])
+
+    else:
+        logger.debug("Couldn't find a pupilghost center, skipping rest of work")
+        return None
+
+    data = science_hdu.data
+
+    #n_samples = 1750
+    logger.debug("Getting sky-samples for science frame")
+    samples = numpy.array(podi_fitskybackground.sample_background(data, None, None, 
+                                                      min_found=n_samples, fit_regions=[], 
+                                                      boxwidth=boxwidth))
+
+    if (samples.shape[0] <= 0):
+        logger.debug("Couldn't find enough sky samples, skipping OTA %s" % (extname))
+        return None
+
+    box_centers = samples[:,2:4]
+
+    dxy = box_centers - center
+    pg_xy = box_centers
+    if (not pg_matched):
+        # This is the case if we compare to the full template
+        # Now convert the x/y from the science frame 
+        # into x/y in the pupil ghost frame
+        pg_xy = dxy + [ 0.5*pg.shape[0], 0.5*pg.shape[1] ]
+        
+    #print dxy[0:10,:]
+    #print pg_xy[0:10,:]
+
+    #print pg_samples.shape
+    #print pg_xy.shape
+    logger.debug("Getting sky-samples for pupilghost template")
+    pg_samples =  numpy.array(podi_fitskybackground.sample_background(pg, None, None, 
+                                                                      fit_regions=[],
+                                                                      box_center=pg_xy,
+                                                                      boxwidth=boxwidth))
+    if (pg_samples.shape[0] <= 0):
+        logger.debug("Couldn't find enough pg.template samples, skipping OTA %s" % (extname))
+        return None
+
+
+    #print pg_samples.shape
+
+    #continue
+
+    ri = 400
+    ro = 3300
+    radius = numpy.sqrt(numpy.sum( dxy**2, axis=1))
+    #print radius[0:10]
+
+    #print extname
+    #print center
+
+    # numpy.savetxt("xxx", samples)
+    # numpy.savetxt("yyy", pg_samples)
+
+    # unaffected data points inside or outside the pupil ghost
+    unaffected = (radius < ri) | (radius > ro)
+    skylevel = numpy.median(samples[:,4][unaffected])
+    logger.debug("Found background level not affected by PG: %.1f" % (skylevel))
+
+    # derive maximum intensity of pupil ghost 
+    # better: read from file
+    pg_max = numpy.max(pg_samples[:,4])
+    logger.debug("Maximum intensity of pupilghost: %f" % (pg_max))
+
+    # Now merge the two arrays:
+    merged = numpy.empty(shape=(pg_xy.shape[0],6))
+    merged[:,0:2] = samples[:,2:4]
+    merged[:,2:4] = pg_samples[:,2:4]
+    merged[:,4] = numpy.array(samples)[:,4] - skylevel
+    merged[:,5] = numpy.array(pg_samples)[:,4]
+    # numpy.savetxt("zzz", merged)
+
+
+    ratio = merged[:,4] / merged[:,5]
+    logger.debug("simple ratio science/template (ota %s): %f" % (extname, numpy.median(ratio)))
+
+    strong_pg_signal = merged[:,5] > 0.4*pg_max
+    valid_ratios = ratio[strong_pg_signal]
+    median_ratio = numpy.median(valid_ratios)
+    uncert_ratio = numpy.std(valid_ratios)
+
+    logger.debug("ratio for strong pg pixels (%d samples): %f +/- %f" % (
+        numpy.sum(strong_pg_signal), median_ratio, uncert_ratio))
+
+    good_samples = None
+    if (numpy.sum(strong_pg_signal) > 0):
+        # Only merge the samples with sufficient pupilghost signal
+        good_samples = merged[strong_pg_signal]
+        
+    return good_samples
+
+
+
+
+
+
+
+
 def subtract_pupilghost_extension(input_hdu, rotator_angle, filtername, pupil_hdu, scaling, 
                                   rotate=True, verbose=True, non_negative=True,
                                   source_center_coords='data'
@@ -197,7 +591,7 @@ def subtract_pupilghost_extension(input_hdu, rotator_angle, filtername, pupil_hd
         if ('PGEFCTVX' in input_hdu.header):
             center_x = input_hdu.header['PGEFCTVX']
     #
-    # by default, resort to the old-fashined canned values
+    # by default, resort to the old-fashioned canned values
     else:
         if (filtername in pupilghost_centers):
             if (extname in pupilghost_centers[filtername]):
@@ -580,6 +974,9 @@ def get_pupilghost_scaling(science_frame, pupilghost_frame,
         return any_affected, clipped_median, clipped_std
 
     return any_affected, -1., -1.
+
+
+
 
 
 
