e93a5040fbec898a490721bb02c53b1c7ad4b5da
Ralf Kotulla <kotulla@wisc.edu>
2014-04-21 16:06:28 -0500

    Some modifications to fix problem with non-sidereally stacked reference frames. Added -mpc flag to query MPC instead of the french VOserver.

--- test/podi_asteroids.py
+++ test/podi_asteroids.py
@@ -15,6 +15,12 @@ import ephem
 import os
 import sys
 
+bn, _ = os.path.split(os.path.abspath(sys.argv[0]))
+sys.path.append(bn+"/../")
+from podi_commandline import *
+import podi_mpchecker
+
+
 mjd0 = + 2400000.5
 
 def get_minorobjects(epoch, ra, dec, sr):
@@ -54,17 +60,19 @@ def get_minorobjects(epoch, ra, dec, sr):
 
 
 
-def create_ds9_region_file(votab, region_file, epoch, mjd_start, mjd_end, rel_nonsidereal):
+def create_ds9_region_file(votab_array, region_file, epoch, mjd_start, mjd_end, rel_nonsidereal):
 
-    obj_ra = votab.array['RA']
-    obj_dec = votab.array['DEC']
-    names = votab.array['Name']
+    obj_ra = votab_array['RA']
+    obj_dec = votab_array['DEC']
+    names = votab_array['Name']
 
     exptime = (mjd_end - mjd_start) * 86400.
 
-    pm_ra = votab.array['dracosdec'] - rel_nonsidereal[0]
-    pm_dec = votab.array['ddec'] - rel_nonsidereal[1]
-
+    print "relative non-sidereal motion:", rel_nonsidereal
+    pm_ra = votab_array['dracosdec'] - rel_nonsidereal[0]
+    pm_dec = votab_array['ddec'] - rel_nonsidereal[1]
+    print pm_ra
+    print pm_dec
     # Now create a ds9 region file with all information
 
     # coord_j2000 = ephem.Equatorial(obj_ra, obj_dec, epoch=ephem.J2000)
@@ -78,6 +86,8 @@ def create_ds9_region_file(votab, region_file, epoch, mjd_start, mjd_end, rel_no
     for obj in range(obj_ra.shape[0]):
 
         coord_j2000 = ephem.Equatorial(obj_ra[obj], obj_dec[obj], epoch=ephem.J2000)
+        cos_declination = math.cos(coord_j2000.dec)
+
         print "  %-13s : %s %s" % (names[obj], coord_j2000.ra, coord_j2000.dec)
         length = numpy.hypot(pm_ra[obj], pm_dec[obj]) * (exptime/3600.) / 3600.
         angle = numpy.degrees(numpy.arctan2(pm_ra[obj], pm_dec[obj])) + 90.
@@ -85,18 +95,29 @@ def create_ds9_region_file(votab, region_file, epoch, mjd_start, mjd_end, rel_no
         offset_ra = (epoch - mjd_start)*24.*pm_ra[obj] if mjd_start > 0 else 0.
         offset_dec = (epoch - mjd_start)*24.*pm_dec[obj] if mjd_start > 0 else 0.
 
-        
+        dec0 = numpy.degrees(coord_j2000.dec) - offset_ra/3600./cos_declination
+        ra0  = numpy.degrees(coord_j2000.ra)  - offset_ra/3600.
+
+        dec1 = dec0 + (exptime/3600.) * pm_dec[obj] / 3600.
+        ra1  = ra0  + (exptime/3600.) * pm_ra[obj] / cos_declination / 3600.
+        print names[obj], offset_ra, offset_dec
+
         dict = {
-            'ra': numpy.degrees(coord_j2000.ra)-offset_ra,
-            'dec': numpy.degrees(coord_j2000.dec)-offset_ra,
+            'ra': numpy.degrees(coord_j2000.ra)-offset_ra/3600.,
+            'dec': numpy.degrees(coord_j2000.dec)-offset_ra/3600.,
             'pm_ra': pm_ra[obj] * (exptime/3600.) / 3600.,
             'pm_dec': pm_dec[obj] * (exptime/3600.) / 3600.,
             'length': length,
             'angle': angle,
             'name': names[obj],
+            'ra0': ra0,
+            'dec0': dec0,
+            'ra1': ra1,
+            'dec1': dec1,
         }
 
-        print >>ds9, '# vector(%(ra)f, %(dec)f, %(length)f, %(angle)f) vector=1' % dict
+#        print >>ds9, '# vector(%(ra)f, %(dec)f, %(length)f, %(angle)f) vector=1' % dict
+        print >>ds9, 'line(%(ra0)f,%(dec0)f,%(ra1)f,%(dec1)f) # line=1 0' % dict
         print >>ds9, '# text(%(ra)f,%(dec)f) text={%(name)s}' % dict
 
     ds9.close()
@@ -141,7 +162,13 @@ if __name__ == "__main__":
 
     fitsfile = sys.argv[1]
 
-    votab = get_asteroid_list_from_fitsfile(fitsfile)
+    if (cmdline_arg_isset("-mpc")):
+        mp_data = podi_mpchecker.get_mpc_catalog(fitsfile)
+        print mp_data
+    else:
+        votab = get_asteroid_list_from_fitsfile(fitsfile)
+        mp_data = votab.array
+
 
     hdulist = astropy.io.fits.open(fitsfile)
     rel_nonsidereal_ra = 0.
@@ -158,12 +185,12 @@ if __name__ == "__main__":
 
     exptime = 3500.
 
-    obj_ra = votab.array['RA']
-    obj_dec = votab.array['DEC']
-    names = votab.array['Name']
+    # obj_ra = votab.array['RA']
+    # obj_dec = votab.array['DEC']
+    # names = votab.array['Name']
 
-    pm_ra = votab.array['dracosdec'] - rel_nonsidereal_ra
-    pm_dec = votab.array['ddec'] - rel_nonsidereal_dec
+    # pm_ra = votab.array['dracosdec'] - rel_nonsidereal_ra
+    # pm_dec = votab.array['ddec'] - rel_nonsidereal_dec
 
     # Now create a ds9 region file with all information
 
@@ -171,7 +198,7 @@ if __name__ == "__main__":
 
     region_file = sys.argv[2]
 
-    create_ds9_region_file(votab, region_file, epoch, mjd_start, mjd_end, [rel_nonsidereal_ra, rel_nonsidereal_dec])
+    create_ds9_region_file(mp_data, region_file, epoch, mjd_start, mjd_end, [rel_nonsidereal_ra, rel_nonsidereal_dec])
 
 
 
