4fe70fb53e97aa3ff5709cc01c29f11a4053c622
Ralf Kotulla <kotulla@wisc.edu>
2014-04-28 10:11:02 -0500

    implemented the new parallelized make_catalog

--- test/podi_asteroidmetry.py
+++ test/podi_asteroidmetry.py
@@ -21,40 +21,7 @@ import scipy.spatial
 
 import podi_sitesetup as sitesetup
 
-
-def make_catalogs(inputlist):
-
-
-    for fitsfile in inputlist:
-        logger.info("Creating source catalog for %s" % (fitsfile))
-        catfile = "%s.cat" % (fitsfile[:-5])
-
-        if (os.path.isfile(catfile)):
-            # Don't do anything if the catalog already exists
-            continue
-
-        sex_config_file = "%s/.config/moving.sex" % (sitesetup.exec_dir)
-        parameters_file = "%s/.config/moving.sexparam" % (sitesetup.exec_dir)
-        sexcmd = "%s -c %s -PARAMETERS_NAME %s -CATALOG_NAME %s %s" % (
-            sitesetup.sextractor, sex_config_file, parameters_file, catfile, 
-            fitsfile)
-        
-        start_time = time.time()
-        try:
-            ret = subprocess.Popen(sexcmd.split(), 
-                                   stdout=subprocess.PIPE, 
-                                   stderr=subprocess.PIPE)
-            (sex_stdout, sex_stderr) = ret.communicate()
-            #os.system(sexcmd)
-            if (ret.returncode != 0):
-                logger.warning("Sextractor might have a problem, check the log")
-                logger.debug("Stdout=\n"+sex_stdout)
-                logger.debug("Stderr=\n"+sex_stderr)
-        except OSError as e:
-            podi_logging.log_exception()
-            print >>sys.stderr, "Execution failed:", e
-        end_time = time.time()
-        logger.debug("SourceExtractor returned after %.3f seconds" % (end_time - start_time))
+import podi_makecatalogs
 
     
 
@@ -206,7 +173,7 @@ def find_moving_objects(sidereal_reference, inputlist, min_count, min_rate, mpcf
 
     logger = logging.getLogger("Astro(id)metry")
 
-    make_catalogs(inputlist)
+    podi_makecatalogs.make_catalogs(inputlist, "moving.sex", "moving.sexparam")
     
     catalog_filelist = []
     catalog = []
@@ -253,7 +220,7 @@ def find_moving_objects(sidereal_reference, inputlist, min_count, min_rate, mpcf
         print catalog_filename, cat_data.shape
 
 
-    max_rate = 200 # arcsec / hour
+    max_rate = 500 # arcsec / hour
     min_distance = 2. / 3600. # arcsec
     motion_rates = numpy.zeros((0,6))
 
