f194542835d82bca24a659decce470a79c85fb67
Ralf Kotulla <kotulla@wisc.edu>
2016-09-09 18:15:51 -0500

    Added stand-alone functionality, printing a small blurb with version information if called with "batch" as first parameter (i.e. version.py batch) only the version, git branch and last commit are printed for easier parsing elsewhere.

--- version.py
+++ version.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 #
 # Update this variable below when changing versions
@@ -55,14 +56,15 @@ def get_last_commit(src_directory=None):
     return commit_hash
 
 
-
+pl_author = "Ralf Kotulla"
+pl_email = "kotulla@wisc.edu"
 def record_pipeline_versioning(header):
 
     header["PLVER"] = (pipeline_plver, "name and version")
     header["PIPELINE"] = (pipeline_name, "pipeline name")
     header["PLVERSIO"] = (pipeline_version, "pipeline version")
-    header["PLAUTHOR"] = ("Ralf Kotulla", "pipeline author")
-    header["PLEMAIL"] = ("kotulla@wisc.edu", "contact email")
+    header["PLAUTHOR"] = (pl_author, "pipeline author")
+    header["PLEMAIL"] = (pl_email, "contact email")
     header['FOR_HELP'] = (odi_help_email, "contact ODI help with questions")
 
     src_directory,_ = os.path.split(__file__)
@@ -77,6 +79,17 @@ def record_pipeline_versioning(header):
     add_fits_header_title(header, "Pipeline information", 'PLVER')
 
 
-
 if __name__ == "__main__":
+
+    git_branch = get_branch_name()
+    git_commit = get_last_commit()
+
+    if (len(sys.argv) > 1 and sys.argv[1] == "batch"):
+        print(pipeline_version, git_branch, git_commit)
+        sys.exit(0)
+
+    print("This is %s version %s (GIT:: branch:%s commit:%s)" % (
+        pipeline_name, pipeline_version, git_branch, git_commit))
+    print("Developed by: %s (%s)" % (pl_author, pl_email))
+    print("For questions, email %s" % (odi_help_email))
     pass
