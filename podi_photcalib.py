ccc86497b27f0eb1a3c522d1ee4e62cc6882751e
Daniel Harbeck <harbeck@noao.edu>
2017-03-01 13:12:08 -0700

    Add hooks to convert PS1 AB magnitudes to SDSS system. Feature is not activated yet.
    See line 936 where to call the correction. Correction in function ps2sdss needs adjustment to index the photometry table correctly.

--- podi_photcalib.py
+++ podi_photcalib.py
@@ -185,7 +185,53 @@ def load_catalog_from_stripe82cat(ra, dec, calib_directory, sdss_filter):
     return std_stars
 
 
-    
+
+def ps2sdss (catalogdata, tonry=False):
+    '''
+    PS1 to SDSS conversion following Tonry et al 2012
+    https://www.google.com/url?q=https%3A%2F%2Farxiv.org%2Fpdf%2F1203.0297.pdf&sa=D&sntz=1&usg=AFQjCNFZ3T88aqAPI32uYeygq7tNybjUHQ
+    The field names of the PS1 magnitude colums are hardwired in here.
+    The input catalog is overwritten, so make sure you do not need the original values any more!
+    Correction only for griz filters!
+
+    Parameters
+    ----------
+    catalogdata -- narray Catalog containing the reference magnitudes in panstarrs AB system
+    tonry -- boolean  If true, use Tonry 2012 conversion from PS1 to SDSS. If false (default), use Finkenbeiner 2016 covnersion.
+
+    '''
+
+
+
+    print "---- WARNING - This method is not included in workflow yet ----"
+
+    # Tonry 2012
+    ps1colorterms = {}
+
+    if tonry:
+        print "converting PS1 to sdss system via tonry"
+        ps1colorterms['REF_G'] = [0.019, 0.145, 0.013]
+        ps1colorterms['REF_R'] = [0.007, 0.004, -0.001]
+        ps1colorterms['REF_I'] = [0.010, 0.011,-0.005]
+        ps1colorterms['REF_Z'] = [-0.012, -0.039, 0.013]
+        psgr = catalogdata['REF_G'] - catalogdata['REF_R']
+    # Finkenbein 2016
+    # http://iopscience.iop.org/article/10.3847/0004-637X/822/2/66/meta#apj522061s2-4 Table 2
+    else:
+        print "converting PS1 to sdss system via finkenbeiner"
+        ps1colorterms['REF_G'] = [-0.01808,-0.13595, 0.01941,-0.00183][::-1]
+        ps1colorterms['REF_R'] = [-0.01836,-0.03577, 0.02612,-0.00558][::-1]
+        ps1colorterms['REF_I'] = [ 0.01170,-0.00400, 0.00066,-0.00058][::-1]
+        ps1colorterms['REF_Z'] = [-0.01062, 0.07529,-0.03592, 0.00890][::-1]
+        psgr = catalogdata['REF_G'] - catalogdata['REF_I']
+
+    for filter in ps1colorterms:
+        colorcorrection = numpy.polyval (ps1colorterms[filter], psgr)
+        if tonry:
+            catalogdata[filter] += colorcorrection
+        else:
+            catalogdata[filter] -= colorcorrection
+
     
 def photcalib_old(fitsfile, output_filename, calib_directory, overwrite_cat=None):
     """
@@ -887,6 +933,10 @@ def photcalib(source_cat, output_filename, filtername, exptime=1,
             )
             if (_std_stars is not None and _std_stars.shape[0] > 0):
                 detailed_return['catalog'] = catname
+                if catname.equals ("panstarrs"):
+                    # TODO: do conversion from panstarrs into SDSS system.
+                    pass
+
                 std_stars = _std_stars
                 logger.info("Found %d reference sources in %s catalog" % (std_stars.shape[0], catname))
                 break
